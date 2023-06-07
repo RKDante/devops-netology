6cf229cd148e1805008a54514ed210b92b5b757b
Ilya Krivov <i.krivov013@gmail.com>
2023-06-07 23:17:22 +0300

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+##!/bin/bash
 # display command line options
 
 count=1
-for param in "$*"; 
-     doecho"\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
+    count=$(( $count + 1 ))
+done
